d28c5b85c199495dccd3b0368a466d2e4b3fc337
learnuidev <learnuidev@gmail.com>
2022-06-06 20:58:46 -0400

    Attribute preds tutorial

--- novus/src/main/novus/schema.clj
+++ novus/src/main/novus/schema.clj
@@ -38,6 +38,29 @@
    - That is, attributes are themselves entities with associated attributes.
    - Datomic defines a set of built-in system attributes that are used to define new attributes.")
 
+
+;; Installing an attribute definition
+(comment
+  "- Attribute definitions are represented as clojure maps (see Data Structures) that
+     can be submitted to a database as part of a transaction.
+   - It is idiomatic to use the transaction map data structure, as shown in the following example.
+   - It defines an attribute named :person/name of type :db.type/string with :db.cardinality/one
+     that is intended to capture a person's name.
+")
+(def person-name-attr
+ {:db/ident :person/name
+  :db/valueType :db.type/string
+  :db/cardinality :db.cardinality/one
+  :db/doc "A person's name"})
+
+(comment
+  "- Leading underscores are used for reverse lookup in pull. If your attribute has a
+     leading underscore then you will not be able to use reverse lookup with that attribute.
+
+   - Attribute installation is atomic. All the required information for an attribute must be specified in a
+     single transaction, after which the attribute will be immediately available for use.")
+
+
 ;; Required schema attributes
 (comment
  "Every new attribute is described by three required attributes:
@@ -177,3 +200,121 @@
       even for attributes with :db/noHistory set to true.")
 
 ;; Lab Time: Unique, Index and Attribute Predicates, isComponent, noHistory
+
+;; ===============================================================================================
+;; ==== Tutorial: Attribute predicates
+
+(def client (d/client {:server-type :dev-local
+                       :system "dev"}))
+
+(comment
+  "3 Database API's"
+  (d/list-databases (d/client {:server-type :dev-local
+                                :system "dev"})
+                    {})
+  (d/create-database (d/client {:server-type :dev-local
+                                :system "dev"})
+                     {:db-name "twitter"})
+  (d/delete-database (d/client {:server-type :dev-local
+                                :system "dev"})
+                    {:db-name "twitter"}))
+
+(comment
+  " Rationale
+  - You may want to constrain an attribute value by more than just its storage/representation
+    type.
+  - For example, an email address is not just a string, but a string with a particular format. In
+    Datomic, you can assert attribute predicates about an attribute.
+  - Attribute predicates are asserted via the :db.attr/preds attribute, and are fully-qualified
+    symbols that name a predicate of a value.
+  - Predicates return true (and only true) to indicate success. All other values indicate
+    failure and are reported back as transaction errors.
+
+  - Inside transactions, Datomic will call all attribute predicates for all attribute values,
+    and abort a transaction if any predicate fails.
+
+    For example, the following function validates that a user-name has a particular length:
+ ")
+
+;; Step 2: Create basic schema
+(defn user-name? [username]
+  (<= 6 (count username) 15))
+
+(comment
+  "To install the user-name? predicate, add a db.attr/preds value to an attribute, e.g.")
+(def user-schema
+  [{:db/ident :user/id
+    :db/doc "User ID"
+    :db/unique :db.unique/identity
+    :db/valueType :db.type/uuid
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :user/username
+    :db/doc "User Name"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db.attr/preds 'novus.schema/user-name?}])
+
+;;
+(def twitter  (d/connect client {:db-name "twitter"}))
+
+;; transact schema
+(comment
+  (d/transact  twitter {:tx-data user-schema}))
+
+;; Transact datas
+(comment
+  (java.util.UUID/randomUUID))
+(comment
+   @error-atom)
+(comment
+  ;; Yay
+  (d/transact twitter {:tx-data [{:user/id #uuid "ecb14c30-cf86-41b5-87a4-6cdb61f73807"
+                                  :user/username "john.doe"}]}))
+
+;;
+(comment
+  "A transaction that includes an invalid user-name will result in an incorrect anomaly that includes:
+     - the entity id
+     - the attribute name
+     - the attribute value
+     - the name of the failed predicate
+     - the predicate return in :db.error/pred-return"
+
+  "For example, the string 'john' is not a valid user-name? and will cause an anomaly like:")
+
+(defonce error-atom (atom nil))
+(comment
+  @error-atom)
+(comment
+  (try
+   (d/transact twitter {:tx-data [{:user/id #uuid "ecb14c30-cf86-41b5-87a4-6cdb61f73807"
+                                   :user/username "john"}]})
+   (catch Exception err
+     (reset! error-atom (ex-data err)))))
+
+
+(def error-message
+  {:cognitect.anomalies/category :cognitect.anomalies/incorrect,
+   :cognitect.anomalies/message [:cognitect.anomalies/message "Entity -9223301668109598081 attribute :user/username value 'john' failed pred novus.superpowers/user-name?"]
+   :db.error/pred-return false,
+   :db/error :db.error/attr-pred})
+
+(comment
+  "
+   - Attribute predicates must be on the classpath of a process that is performing a transaction.
+   - Attribute predicates can be asserted or retracted at any time, and will be enforced starting
+     on the transaction after they are asserted.
+   - Asserting or retracting an attribute predicate has no effect on attribute values that already
+     exist in the database.
+   - Attribute predicates can cancel the transaction directly.")
+
+;; Summary
+(comment
+  "Summary
+   - Attribute predicates are predicate functions allow you to constrain an attribute value
+   - Attribute predicates are asserted via the :db.attr/preds attribute, and are fully-qualified
+     symbols that name a predicate of a value.
+   - Predicates return true (and only true) to indicate success. All other values indicate
+     failure and are reported back as transaction errors.
+   - Attribute predicates must be on the classpath of a process that is performing a transaction.")
