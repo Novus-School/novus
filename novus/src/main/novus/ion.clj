9456d6519820cd349801715e00291babf6520a8d
learnuidev <learnuidev@gmail.com>
2022-06-09 20:57:44 -0400

    Added changes

--- novus/src/main/novus/ion.clj
+++ novus/src/main/novus/ion.clj
@@ -2,14 +2,16 @@
  (:require [integrant.core :as ig]
            [novus.components.auth0 :as auth0]
            [novus.components.datomic-cloud :as datomic-cloud]
-           ; [datomic.ion :as ion]
+           [datomic.ion :as ion]
            [novus.server :as server]))
 
 (def integrant-setup
   {::server/app {:datomic (ig/ref ::datomic-cloud/db)
                  :auth0 (ig/ref ::auth0/auth)}
-   ; ::auth0/auth {:client-secret (get (ion/get-params {:path "/datomic-shared/prod/cheffy/"}) "auth0-client-secret")}
-   ::auth0/auth {:client-secret "auth0-client-secret"}
+   ; ::auth0/auth {:client-secret (get (ion/get-params {:path "/datomic-shared/prod/novus/"}) "auth0-client-secret")
+   ;               :client-id (get (ion/get-params {:path "/datomic-shared/prod/novus/"}) "auth0-client-id")}
+   ::auth0/auth {:client-id "auth0-client-id"
+                 :client-secret "auth0-client-secret"}
    ::datomic-cloud/db {:server-type :ion
                        :region "us-east-1"
                        :system "novus-prod"
